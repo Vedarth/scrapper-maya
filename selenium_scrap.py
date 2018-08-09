f8d18213417bab599a5871b21389ef1678183bca
Vedarth <vedarth.sharma@gmail.com>
2018-08-09 22:10:38 +0530

    Removed debug code

--- selenium_scrap.py
+++ selenium_scrap.py
@@ -13,20 +13,19 @@ load_dotenv(dotenv_path=env_path)
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.ehlo()
 server.login(os.getenv('email'), os.getenv('password'))
-prev_name1, prev_name2, prev_date = "", "", ""
 
 SENDER= os.getenv('email')
 PASS= os.getenv('password')
 RECEIVER= os.getenv('reciever')
 PATH='/app/phantomjs'
+prev_name1, prev_name2, prev_date = "", "", ""
 
 while True:
     driver = webdriver.PhantomJS(executable_path=PATH)
     driver.get("https://maya.tase.co.il/")
     driver.set_window_size(1920, 1080)
     sleep(5)
-    lucky_button = driver.find_element_by_id("searchDesktop")
-    print(driver.page_source)    
+    lucky_button = driver.find_element_by_id("searchDesktop")    
     lucky_button.send_keys('קנאביס')
     lucky_button.send_keys(Keys.ENTER)
     sleep(5)    
@@ -40,7 +39,8 @@ while True:
             msg = "\n New data found "+name1.text
         
             server.sendmail(SENDER, RECIEVER, msg.encode('utf-8'))
+            print("Email sent")
     
     prev_name1, prev_name2, prev_date = name1.text, name2.text, date.text
-    driver.quit()        
+    driver.quit()
     sleep(55)
